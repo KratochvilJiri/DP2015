e5f0ece921a00c1a3e4cc3da34a72d5131443a44
Jiří Kratochvíl <juurakratochvil@gmail.com>
2015-09-30 16:17:10 +0200

    Initialisation - simple application for add/delete "nerds" to database - tutorial.

---
+++ config/db.js
@@ -0,0 +1,5 @@
+// config/db.js
+
+module.exports = {
+    url : 'mongodb://admin:juromil@ds040888.mongolab.com:40888/excel_fit'
+}
---
+++ public/js/controllers/NerdCtrl.js
@@ -0,0 +1,41 @@
+// public/js/controllers/NerdCtrl.js
+angular.module('NerdCtrl', []).controller('NerdController', function($scope,$http) {
+
+    $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/nerds')
+        .success(function(data) {
+            $scope.nerds = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createNerd = function() {
+        $http.post('/api/nerds', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.nerds = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteNerd = function(id) {
+        $http.delete('/api/nerds/' + id)
+            .success(function(data) {
+                $scope.nerds = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+});
