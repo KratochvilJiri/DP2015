52f9314ec4a1576defa6c9a1de83e216e67ecbf0
Jiří Kratochvíl <juurakratochvil@gmail.com>
2016-04-24 19:00:02 +0200

    Helpdesk filters

--- public/js/controllers/helpdesk/OverviewCtrl.js
+++ public/js/controllers/helpdesk/OverviewCtrl.js
@@ -1,6 +1,27 @@
 // probably useless
-angular.module('OverviewCtrl', []).controller('OverviewController', ['$scope', 'IssueService','$filter', function($scope, IssueService, $filter) {
+angular.module('OverviewCtrl', []).controller('OverviewController', ['$scope', 'IssueService', '$filter', function($scope, IssueService, $filter) {
 
+    $scope.filter = {};
+
+    $scope.priorities = [
+        { constant: undefined, text: "Vše", color: "" },
+        { constant: 'LOW', text: "Nízká", color: "yellow" },
+        { constant: 'NORMAL', text: "Střední", color: "orange" },
+        { constant: 'HIGH', text: "Vysoká", color: "red" },
+    ];
+
+    $scope.types = [
+        { constant: undefined, text: "Vše", color: "" },
+        { constant: 'SYS_ERR', text: "Systémová chyba", color: "red" },
+        { constant: 'QUESTION', text: "Otázka", color: "blue" },
+        { constant: 'IMPROVEMENT_SUGG', text: "Návrh na zlepšení", color: "green" },
+    ];
+
+    $scope.states = [
+        { constant: undefined, text: "Vše", color: "" },
+        { constant: 'IN_PROGRESS', text: "V řešení", color: "red" },
+        { constant: 'DONE', text: "Vyřešeno", color: "green" },
+    ]
     //
     var getIssues = function() {
         IssueService.getAll()
@@ -23,7 +44,7 @@ angular.module('OverviewCtrl', []).controller('OverviewController', ['$scope', '
         var today = $filter('date')(new Date(), "yyyy-MM-dd'T'HH:mm:ss.sss'Z'");
         var timestamp1 = new Date(today);
         $scope.issues.forEach(function(issue) {
-            if(!issue.supervisor){
+            if (!issue.supervisor) {
                 issue.supervisor = {};
                 issue.supervisor.name = "nepřiřazeno";
             }
@@ -31,13 +52,25 @@ angular.module('OverviewCtrl', []).controller('OverviewController', ['$scope', '
             var diff = timestamp1 - timestamp2;
 
             $scope.dayRemaining = Math.floor(diff / 86400000);
-            if($scope.dayRemaining == 0){
+            if ($scope.dayRemaining == 0) {
                 issue.daysOpened = 1;
             }
-            else{
+            else {
                 issue.daysOpened = $scope.dayRemaining;
             }
         })
+    }
+
+    $scope.filterState = function(state) {
+        $scope.filter.state = state;
+    }
+
+    $scope.filterType = function(type) {
+        $scope.filter.type = type;
+    }
+
+    $scope.filterPriority = function(priority) {
+        $scope.filter.priority = priority;
     }
 
     getIssues();
