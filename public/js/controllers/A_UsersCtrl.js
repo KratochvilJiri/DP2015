cd2bda628e5d53d6170ade4d678f8d5d825e7d35
Jiří Kratochvíl <juurakratochvil@gmail.com>
2016-04-24 11:13:19 +0200

    Users - role filter.

--- public/js/controllers/A_UsersCtrl.js
+++ public/js/controllers/A_UsersCtrl.js
@@ -1,38 +1,57 @@
-angular.module('AUsersCtrl',[]).controller('AUsersController',['$scope', 'UserService', function($scope, UserService){
+angular.module('AUsersCtrl', []).controller('AUsersController', ['$scope', 'UserService', function($scope, UserService) {
 
-	// getAll users - every page-load
-	var loadUsers = function (){
+    $scope.filter = {};
+
+    $scope.roles = [];
+
+    $scope.roles2 = [
+        { constant: undefined, text: "Vše" },
+        {constant: "ADMINISTRATOR", text: "Administrátor" },
+        { constant: "PARTICIPANT", text: "Účastník" },
+        { constant: "CONTACT_PERSON", text: "Kontaktní osoba" }
+    ]
+
+    $scope.roles["ADMINISTRATOR"] = "Administrátor";
+    $scope.roles["PARTICIPANT"] = "Účastník";
+    $scope.roles["CONTACT_PERSON"] = "Kontaktní osoba";
+    
+    $scope.filterRole = function (role) {
+         $scope.filter.role = role;
+    }
+
+    // getAll users - every page-load
+    var loadUsers = function() {
         UserService.getAll()
-	    .success(function(data,status,headers,config){
-            if(data.isValid){
-                $scope.users = data.data;	    
-            }
-		    else{
-                // error
-            }
-	    })
-	    .error(function(data, status){
-		    console.error('Error: ', status, data.error);
-	    });
-    } 
+            .success(function(data, status, headers, config) {
+                if (data.isValid) {
+                    $scope.users = data.data;
+                }
+                else {
+                    // error
+                }
+            })
+            .error(function(data, status) {
+                console.error('Error: ', status, data.error);
+            });
+    }
 
- 	// remove user by ID
- 	$scope.removeUser = function(userID){
- 		UserService.delete(userID)
- 		.success(function(data){
-             if(data.isValid){
-                 loadUsers();
-                 $scope.showSuccess("Uživatel byl úspěčně odstraněn");
-             }
-             else{
-                 $scope.showError(data.errors);
-             }
- 			$scope.users = data.data;
- 		})
- 		.error(function(data, status){
- 			console.error('Error: ', status, data.error);
- 		});
- 	}
-     
-     loadUsers();
+    // remove user by ID
+    $scope.removeUser = function(userID) {
+        UserService.delete(userID)
+            .success(function(data) {
+                if (data.isValid) {
+                    loadUsers();
+                    $scope.showSuccess("Uživatel byl úspěčně odstraněn");
+                }
+                else {
+                    $scope.showError(data.errors);
+                }
+                $scope.users = data.data;
+            })
+            .error(function(data, status) {
+                console.error('Error: ', status, data.error);
+            });
+    }
+
+    loadUsers();
 }]);
