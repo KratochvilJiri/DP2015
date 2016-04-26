bdcba07c830961fb3d9ec84963f9f1e8c5d4b920
Jiří Kratochvíl <juurakratochvil@gmail.com>
2016-04-26 13:22:33 +0200

    Administration - conference - possible attachement types for adding.

--- public/js/controllers/ConferenceCtrl.js
+++ public/js/controllers/ConferenceCtrl.js
@@ -1,114 +1,139 @@
-angular.module('ConferenceCtrl',[]).controller('ConferenceController',['$scope','$timeout','$state', 'ConferenceService','SessionService' ,function($scope,$timeout,$state, ConferenceService, SessionService){
+angular.module('ConferenceCtrl', []).controller('ConferenceController', ['$scope', '$timeout', '$state', 'ConferenceService', 'SessionService', function($scope, $timeout, $state, ConferenceService, SessionService) {
     $scope.session = SessionService;
     $scope.conference = {};
     $scope.conference.sponsorshipLevels = [];
     $scope.allConference = [];
-    
+
     var loadAllConference = function() {
         ConferenceService.getAll()
-        .success(function(data){
-            if(data.isValid){
-                $scope.allConference = data.data;
-                setActiveConference();
-            }
-            else{
-                $scope.showErrors(data.errors);
+            .success(function(data) {
+                if (data.isValid) {
+                    $scope.allConference = data.data;
+                    setActiveConference();
+                }
+                else {
+                    $scope.showErrors(data.errors);
+                }
+            })
+            .error(function(data, status) {
+                console.log('Error: ', status, data.error);
+            });
+    }
+
+    var getAttachementTypesForLevel = function() {
+        var isAssigned = true;
+        $scope.conference.sponsorshipLevels.forEach(function(level) {
+            level.possibleAttachementTypes = [];
+            // console.log(level);
+            $scope.conference.attachementTypes.forEach(function(type) {
+                level.attachementTypes.forEach(function(attachementTypeInLevel) {
+                    console.log(type);
+                    console.log(attachementTypeInLevel);
+                    if (type.hash == attachementTypeInLevel.hash) {
+                        isAssigned = false;
+                    }
+                })
+                if (isAssigned) {
+                    level.possibleAttachementTypes.push(type);
+                }
+                else {
+                    isAssigned = true;
+                }
+            })
+        })
+        console.log($scope.conference.sponsorshipLevels);
+    }
+
+    var setActiveConference = function() {
+        $scope.allConference.forEach(function(conference) {
+            if (conference.active) {
+                $scope.conference = conference;
+                getAttachementTypesForLevel();
             }
         })
-        .error(function(data,status) {
-            console.log('Error: ', status, data.error);
-        });
     }
-    
-    var setActiveConference = function () {
-       $scope.allConference.forEach(function (conference) {
-           if(conference.active){
-               $scope.conference = conference;
-           }
-       }) 
-    }
-    
-    $scope.addConference = function () {
+
+    $scope.addConference = function() {
         $scope.conference = {};
     }
-    
+
     // add sponsorShipLevel
-    $scope.addSponsorshipLevel = function () {
-        if(!$scope.conference.sponsorshipLevels)
+    $scope.addSponsorshipLevel = function() {
+        if (!$scope.conference.sponsorshipLevels)
             $scope.conference.sponsorshipLevels = [];
-        
+
         $scope.conference.sponsorshipLevels.push({});
     }
-    
+
     // remove sponsorShipLevel
-    $scope.removeSponsorshipLevel = function (index){
-        $scope.conference.sponsorshipLevels.splice(index,1);
+    $scope.removeSponsorshipLevel = function(index) {
+        $scope.conference.sponsorshipLevels.splice(index, 1);
     }
-    
+
     // add new document to conference
-    $scope.addAttachementType = function () {
-        if(!$scope.conference.attachementTypes)
+    $scope.addAttachementType = function() {
+        if (!$scope.conference.attachementTypes)
             $scope.conference.attachementTypes = [];
-        
+
         var GUID = guid();
         $scope.conference.attachementTypes.push({
             hash: GUID
         });
     }
-    
+
     // remove document
-    $scope.removeAttachementType = function (index){
+    $scope.removeAttachementType = function(index) {
         removeAttachTypeFromSponsorshipLvl(index);
-        $scope.conference.attachementTypes.splice(index,1);
+        $scope.conference.attachementTypes.splice(index, 1);
     }
-    
+
     // save conference
-    $scope.save = function () {
+    $scope.save = function() {
         $scope.conference.active = true;
         SessionService.currentUser.conferenceID = $scope.conference._id;
         console.log($scope.conference);
         //console.log($scope.conference.attachementTypes);
         ConferenceService.save($scope.conference)
-        .success(function(data){
-            if(data.isValid){
-                $scope.showSuccess("Konference byla úspěšně aktualizována/přidána");
-                $state.go('home.dashboard');
-                //loadAllConference();
-            }
-            else{
-                $scope.showErrors(data.errors);
-            }
-        })
-        .error(function(data, status){
- 			console.log('Error: ', status, data.error);
- 		});
+            .success(function(data) {
+                if (data.isValid) {
+                    $scope.showSuccess("Konference byla úspěšně aktualizována/přidána");
+                    $state.go('home.dashboard');
+                    //loadAllConference();
+                }
+                else {
+                    $scope.showErrors(data.errors);
+                }
+            })
+            .error(function(data, status) {
+                console.log('Error: ', status, data.error);
+            });
     }
-    
+
     // remove deleted Attachement from SponsorshipLevels
-    var removeAttachTypeFromSponsorshipLvl = function (index) {     
+    var removeAttachTypeFromSponsorshipLvl = function(index) {
         $scope.conference.sponsorshipLevels.forEach(function(sponsorShipLevel) {
-            if(sponsorShipLevel.attachementTypes){
-                sponsorShipLevel.attachementTypes.forEach(function (attachementType, index, attachementTypes) {
-                    if(attachementType.hash === $scope.conference.attachementTypes[index].hash){
-                        attachementTypes.splice(index,1);
+            if (sponsorShipLevel.attachementTypes) {
+                sponsorShipLevel.attachementTypes.forEach(function(attachementType, index, attachementTypes) {
+                    if (attachementType.hash === $scope.conference.attachementTypes[index].hash) {
+                        attachementTypes.splice(index, 1);
                     }
                 })
             }
         })
     }
-    
+
     loadAllConference();
-    
-   
-  // unique hash creator
-  function guid() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
+
+
+    // unique hash creator
+    function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
     }
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
-  }
-       
+
 }]);
